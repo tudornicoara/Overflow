7837922914559347da4ed1a0cecb675f86f60e40
Tudor Nicoara <nicoaratudor@gmail.com>
2026-06-13 15:44:16 +0300

    Navbar and side menus

--- webapp/src/app/layout.tsx
+++ webapp/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Providers from "@/components/Providers";
+import TopNav from "@/components/nav/TopNav";
 
 export const metadata: Metadata = {
   title: "Overflow",
@@ -13,10 +14,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>
+    <html lang="en" className="h-full">
+      <body className="flex flex-col bg-stone-200 h-full">
         <Providers>
-          {children}
+          <TopNav />
+          <div className="flex grow overflow-auto">
+            <aside className='basis-1/6 shrink-0 border-r border-neutral-500 pt-20 sticky top-0 px-6'>
+              Side menu
+            </aside>
+            <main className="flex-1 pt-20 h-full">
+              { children}
+            </main>
+            <aside className='basis-1/4 shrink-0 px-6 pt-20 bg-stone-300 sticky top-0'>
+              Right content
+            </aside>
+          </div>
         </Providers>
       </body>
     </html>
---
+++ webapp/src/components/nav/TopNav.tsx
@@ -0,0 +1,37 @@
+﻿import Link from "next/link";
+import {AcademicCapIcon, MagnifyingGlassIcon} from "@heroicons/react/24/solid";
+import {Button, Input} from "@heroui/react";
+
+export default function TopNav() {
+    return (
+        <header className="p-2 w-full fixed top-0 z-50 border-b bg-white">
+            <div className="flex px-10 mx-auto">
+                <div className="flex items-center gap-6">
+                    <Link href='/' className='flex items-center gap-3 max-h-16'>
+                        <AcademicCapIcon className="size-10 text-secondary" />
+                        <h3 className="text-xl font-semibold uppercase">Overflow</h3>
+                    </Link>
+                    <nav className="flex gap-3 my-2 text-md text-neutral-500">
+                        <Link href='/'>About</Link>
+                        <Link href='/'>Products</Link>
+                        <Link href='/'>Contact</Link>
+                    </nav>
+                </div>
+                
+                <div className="relative ml-6 flex items-center flex-1">
+                    <MagnifyingGlassIcon className="absolute left-3 size-5 text-neutral-400 pointer-events-none" />
+                    <Input
+                        className="pl-9 w-full"
+                        type="search"
+                        placeholder="Search"
+                    />
+                </div>
+                
+                <div className="flex basis-1/4 shrink-0 justify-end gap-3">
+                    <Button variant="outline">Login</Button>
+                    <Button variant="primary">Register</Button>
+                </div>
+            </div>
+        </header>
+    );
+}
